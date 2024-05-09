2327eeae465cdd177839f3e34172881801c519c5
Angelo-Pedott-apel <angelopedott@gmail.com>
2024-05-09 15:42:34 -0300

    update to having a character class

--- Character.cs
+++ Character.cs
@@ -1,4 +1,206 @@
-public class character(string name, string buildname, int buildint, string classname, int classint, List<int> stats, int joblvl, int baselvl,int statpoints, int talentPoints){
+using System.Runtime.InteropServices;
+
+namespace CharacterNamespace
+{
+    public class Character
+    {   
+        public string classname;
+        public string buildname;
+        public string charname;
+        public int buildint;
+        public int classint;
+        public List<int> stats = [1,1,1,0];
+        public int joblevel;
+        public int baselevel;
+        public int statPoints = 0;
+        public int talentPoints = 0;
+        public List<decimal> buildmulti = [0m,0m,0m];
+
+        public Character(string charname, string buildname, int buildint, string classname, int classint)
+        {
+            this.charname = charname;
+            this.buildname = buildname;
+            this.buildint = buildint;
+            this.classname = classname;
+            this.classint = classint;
+            this.ClassDefaults();
+            this.BuildDefaults();
+        }
+
+        public void DisplayCharacter()
+        {
+            Console.WriteLine($"Character name : {charname}");
+            Console.WriteLine($"Class : {classname}");
+            Console.WriteLine($"Build Type : {buildname}");
+            StatsDisplay();
+            CreationChances();
+        }
+
+        int StatCost(int attribute)
+        {
+            int cost = 999;
+            if (attribute < 130)
+            {
+                if (attribute <= 99)
+                {
+                    cost = (int)Math.Floor((decimal)(((attribute - 1) / 10) + 2));
+                }
+                else
+                {
+                    cost = 4 * (int)Math.Floor((decimal)((attribute - 100) / 5)) + 16;
+                }
+            }
+            return cost;
+        }
+
+        decimal StatEfficiency(int statcost, decimal multiplier)
+        {
+            return multiplier / statcost;
+        }
+
+        void ClassDefaults()
+        {
+            switch (classint)
+            {
+                case 1:
+                    statPoints = 1273;
+                    joblevel = 50;
+                    baselevel = 99;
+                    break;
+
+                case 2:
+                    statPoints = 1325;
+                    joblevel = 70;
+                    baselevel = 99;
+                    break;
+
+                case 3:
+                    statPoints = 2597;
+                    joblevel = 70;
+                    baselevel = 200;
+                    break;
+
+                case 4:
+                    statPoints = 4151;
+                    talentPoints = 97;
+                    stats[3] = 100;
+                    joblevel = 50;
+                    baselevel = 250;
+                    break;
+            }
+        }
+
+        void BuildDefaults()
+        {
+            switch (buildint)
+            {
+                case 1:
+                    buildmulti[0] = 0.1m;
+                    buildmulti[1] = 0.05m;
+                    buildmulti[2] = 0.1m;
+                    break;
+                case 2:
+                    buildmulti[0] = 0.5m;
+                    buildmulti[1] = 1m;
+                    buildmulti[2] = 1m;
+                    break;
+                case 3:
+                    for (int i = 0; i < 129; i++)
+                    {
+                        statPoints -= StatCost(stats[2]);
+                        stats[2] += 1;
+                        statPoints -= StatCost(stats[0]);
+                        stats[0] += 1;
+                    }
+                    break;
+            }
+        }
+        public void DistributePoints()
+        {
+            decimal inteff = StatEfficiency(StatCost(stats[0]), buildmulti[1]);
+            decimal dexeff = StatEfficiency(StatCost(stats[1]), buildmulti[0]);
+            decimal lukeff = StatEfficiency(StatCost(stats[2]), buildmulti[2]);
+            while (StatCost(stats[0]) < statPoints || StatCost(stats[1]) < statPoints || StatCost(stats[2]) < statPoints)
+            {
+                if (inteff > dexeff && inteff > lukeff)
+                {
+                    statPoints -= StatCost(stats[0]);
+                    stats[0] += 1;
+                }
+                else
+                {
+                    if (dexeff > lukeff)
+                    {
+                        statPoints -= StatCost(stats[1]);
+                        stats[1] += 1;
+                    }
+                    else
+                    {
+                        statPoints -= StatCost(stats[2]);
+                        stats[2] += 1;
+                    }
+                }
+                inteff = StatEfficiency(StatCost(stats[0]), buildmulti[1]);
+                dexeff = StatEfficiency(StatCost(stats[1]), buildmulti[0]);
+                lukeff = StatEfficiency(StatCost(stats[2]), buildmulti[2]);
+
+                if (StatCost(stats[0]) > statPoints) { inteff = -90m; }
+                if (StatCost(stats[1]) > statPoints) { dexeff = -90m; }
+                if (StatCost(stats[2]) > statPoints) { lukeff = -90m; }
+            }
+        }
+
+        void StatsDisplay()
+        {
+            Console.WriteLine($"\nStats:");
+            if (stats[0] != 0) { Console.WriteLine($"Int = {stats[0]}"); }
+            if (stats[1] != 0) { Console.WriteLine($"Dex = {stats[1]}"); }
+            if (stats[2] != 0) { Console.WriteLine($"Luk = {stats[2]}"); }
+            Console.WriteLine($"StatPoints left = {statPoints}");
+
+            if (stats[3] != 0)
+            {
+                Console.WriteLine("\nTalents:");
+                Console.WriteLine($"CRT = {stats[3]}");
+                Console.WriteLine($"TalentPoints left = {talentPoints}");
+            }
+        }
+
+        void CreationChances()
+        {
+            Console.WriteLine("\n--Creation chances--");
+
+            if (classint >= 1)
+            {
+                decimal ppchance = 30 + 10 + 5 + joblevel * 0.2m + stats[0] * 0.05m + stats[1] * 0.1m + stats[2] * 0.1m;
+                decimal ppchancemin = ppchance - 15;
+                decimal ppchancemax = ppchance - 5;
+                Console.WriteLine($"Prepare potion chance: {ppchancemin} ~ {ppchancemax} %");
+            }
+
+            if (classint >= 3)
+            {
+                decimal spchance = stats[0] + (stats[1] / 2) + stats[2] + joblevel + 50 + (baselevel - 100);
+                decimal spchancemin = spchance + 30 + 40;
+                decimal spchancemax = spchance + 150 + 100;
+                Console.WriteLine($"Special pharmacy creation: {spchancemin} ~ {spchancemax}");
+
+                decimal mcchance = (joblevel / 4) + (stats[1] / 3) + (stats[2] / 2);
+                decimal mcchancemin = mcchance - 150 - 15;
+                decimal mcchancemax = mcchance - 30 - 15;
+                Console.WriteLine($"Mixed Cooking creation: {mcchancemin} ~ {mcchancemax}");
+            }
+
+            if (classint == 4)
+            {
+                decimal bpchance = ((baselevel - 200) * 4) + stats[0] + stats[2] + (stats[3] * 2) + 100;
+                decimal bpchancemin = bpchance + 300;
+                decimal bpchancemax = bpchance + 400;
+                Console.WriteLine($"Bionic pharmacy creation: {bpchancemin} ~ {bpchancemax}");
+            }
+            Console.WriteLine();
+        }
 
 
+    }
 }
--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using CharacterNamespace;
 
 Dictionary<int, string> builddic = new Dictionary<int, string>();
 builddic.Add(1, "PP + Mixed Coooking");
@@ -11,8 +11,8 @@ classdic.Add(2, "Biochemist");
 classdic.Add(3, "Geneticist");
 classdic.Add(4, "Biolo");
 
-
 int choice = 0;
+List<Character> charlist = [];
 while (choice != 4)
 {
     Console.WriteLine("--welcome to an ragnarok pure potter build calculator--");
@@ -33,38 +33,37 @@ while (choice != 4)
 
         case 1:
             Console.WriteLine("you chose to create a new character, good!");
-            string sure = "n";
-            string name = "";
+
+            string charname = "";
             string buildname = "";
             int buildint = 0;
             string classname = "";
             int classint = 0;
-            int joblevel = 1;
-            int baselevel = 1;
-            List<int> stats = [1, 1, 1, 0]; // INT, DEX, LUK ,CRT
 
-            while (sure == "n")
+            string textanswer = "n";
+            while (textanswer == "n")
             {
-                Console.Write("State your build/character name: ");
-                name = Console.ReadLine() ?? "-";
-                Console.Write($"{name} is that right? (y/n): ");
-                sure = Convert.ToString(Console.ReadLine())!;
-                while (sure != "n" && sure != "y")
+                Console.Write("\nState your build/character name: ");
+                charname = Console.ReadLine()!;
+                Console.Write($"{charname} is that right? (y/n): ");
+                textanswer = Convert.ToString(Console.ReadLine())!;
+
+                while (textanswer != "n" && textanswer != "y")
                 {
-                    Console.Write($"Invalid answer, {buildname} is that right? (y/n):");
-                    sure = Convert.ToString(Console.ReadLine())!;
+                    Console.Write($"Invalid answer, {charname} is that right? (y/n):");
+                    textanswer = Convert.ToString(Console.ReadLine())!;
                 }
             }
 
-            sure = "n";
-            while (sure == "n")
+            textanswer = "n";
+            while (textanswer == "n")
             {
-                Console.WriteLine($"What kind of build will {name} be? (chose one):");
+                Console.WriteLine($"\nWhat kind of build will {charname} be? (chose one):");
                 Console.WriteLine("1 - PP (Pure potter) (100% Prepare potion + mixed cooking* )");
                 Console.WriteLine("2 - PP (100% Special pharmacy)");
                 Console.WriteLine("3 - Bionic Pharmacy (luk + dex + crit)");
 
-                while (!Int32.TryParse(Console.ReadLine(), out buildint) || (buildint < 1 && buildint > 4))
+                while (!Int32.TryParse(Console.ReadLine(), out buildint) || buildint < 1 || buildint > 4)
                 {
                     Console.WriteLine("invalid choice");
                 }
@@ -72,68 +71,80 @@ while (choice != 4)
                 builddic.TryGetValue(buildint, out buildname!);
 
                 Console.Write($"{buildname} is that right? (y/n):");
-                sure = Convert.ToString(Console.ReadLine())!;
-                while (sure != "n" && sure != "y")
+                textanswer = Convert.ToString(Console.ReadLine())!;
+                while (textanswer != "n" && textanswer != "y")
                 {
                     Console.Write($"Invalid answer, {buildname} is that right? (y/n):");
-                    sure = Convert.ToString(Console.ReadLine())!;
+                    textanswer = Convert.ToString(Console.ReadLine())!;
                 }
             }
 
-            sure = "n";
-            while (sure == "n")
+            if (buildint == 3)
             {
-                if (buildint == 3)
+                classint = 4;
+                classdic.TryGetValue(classint, out classname!);
+            }
+            else
+            {
+                textanswer = "n";
+                while (textanswer == "n")
                 {
-                    classint = 4;
-                    classdic.TryGetValue(classint, out classname!);
-                    sure = "y";
-                }
-                else
-                {
-                    Console.WriteLine($"What class is {name}? (chose one)");
-                    Console.WriteLine("1 - Alchemist");
-                    Console.WriteLine("2 - Biochemist");
-                    Console.WriteLine("3 - Geneticist");
-                    Console.WriteLine("4 - Biolo\n");
-                    
-                    while (!Int32.TryParse(Console.ReadLine(), out classint) 
-                    || (classint < 1 && classint > 4) 
-                    || ((classint == 1 || classint == 2) && (buildint == 2 || buildint == 3)) 
-                    || (classint == 1 || classint == 2 || classint == 3) 
-                    && (buildint == 3))
+                    Console.WriteLine($"What class is {charname}? (chose one)");
+                    int index = 0;
+                    if (buildint == 1)
                     {
-                        classdic.TryGetValue(classint, out classname!);
-                        if ((classint < 1 && classint > 4)|| classname == null)
+                        index += 1;
+                        Console.WriteLine($"{index} - Alchemist");
+                        index += 1;
+                        Console.WriteLine($"{index} - Biochemist");
+                    }
+                    if (buildint <= 2)
+                    {
+                        index += 1;
+                        Console.WriteLine($"{index} - Geneticist");
+                        index += 1;
+                        Console.WriteLine($"{index} - Biolo");
+                    }
+
+                    while (!Int32.TryParse(Console.ReadLine(), out classint) || classint < 1 || classint > index)
+                    {
+                        classdic.TryGetValue(classint + (4 - index), out classname!);
+                        if ((classint < 1 && classint > index) || classname == null)
                         {
                             Console.WriteLine("invalid choice, try again.");
                         }
-                        if ((classint == 1 || classint == 2) && (buildint == 2 || buildint == 3))
-                        {
-                            Console.WriteLine($"{classname} don't have the correct skill for this buildtype");
-                        }
-
                     }
 
-                    classdic.TryGetValue(classint, out classname!);
+                    classdic.TryGetValue(classint + (4 - index), out classname!);
 
                     Console.WriteLine($"{classname} is that right? (y/n):");
-                    sure = Convert.ToString(Console.ReadLine())!;
-                    while (sure != "n" && sure != "y")
+                    textanswer = Convert.ToString(Console.ReadLine())!;
+                    while (textanswer != "n" && textanswer != "y")
                     {
                         Console.Write($"Invalid answer, {classname} is that right? (y/n):");
-                        sure = Convert.ToString(Console.ReadLine())!;
+                        textanswer = Convert.ToString(Console.ReadLine())!;
                     }
+                    classint += 4-index;
                 }
+
             }
 
-            OptimizeBuild(buildint, classint, stats);
-
-            CreationChances(stats, classint, joblevel);
-
+            Character character = new Character(charname,buildname,buildint,classname,classint);
+            charlist.Add(character);
+            character.DisplayCharacter();
+            
             break;
         case 2:
             Console.WriteLine("you chose to review builds, good!");
+            if (charlist.Count() == 0) {Console.WriteLine("you don't have any build now, create one!");}
+            else{Console.WriteLine($"You got {charlist.Count()} characters choose one:");}
+            
+            int charindex = 1;
+            foreach (Character cha in charlist)
+            {   
+                Console.WriteLine($"{charindex} - {cha.charname}, {cha.classname}, {cha.buildname}");
+            }
+            Console.WriteLine(charlist);
             break;
         case 3:
             Console.WriteLine("you chose to delete builds, good!");
@@ -143,182 +154,16 @@ while (choice != 4)
             break;
         default:
             break;
-        //end
+            //end
 
-            List<int> DistributePoints(List<int> stats, decimal dexmulti, decimal intmulti, decimal lukmulti, int statPoints, int talentPoints)
-            {
-                decimal inteff = StatEfficiency(StatCost(stats[0]), intmulti);
-                decimal dexeff = StatEfficiency(StatCost(stats[1]), dexmulti);
-                decimal lukeff = StatEfficiency(StatCost(stats[2]), lukmulti);
-                while (StatCost(stats[0]) < statPoints || StatCost(stats[1]) < statPoints || StatCost(stats[2]) < statPoints)
-                {
-                    if (inteff > dexeff && inteff > lukeff)
-                    {
-                        statPoints -= StatCost(stats[0]);
-                        stats[0] += 1;
-                    }
-                    else
-                    {
-                        if (dexeff > lukeff)
-                        {
-                            statPoints -= StatCost(stats[1]);
-                            stats[1] += 1;
-                        }
-                        else
-                        {
-                            statPoints -= StatCost(stats[2]);
-                            stats[2] += 1;
-                        }
-                    }
-                    inteff = StatEfficiency(StatCost(stats[0]), intmulti);
-                    dexeff = StatEfficiency(StatCost(stats[1]), dexmulti);
-                    lukeff = StatEfficiency(StatCost(stats[2]), lukmulti);
+            
 
-                    if (StatCost(stats[0]) > statPoints) { inteff = -90m; }
-                    if (StatCost(stats[1]) > statPoints) { dexeff = -90m; }
-                    if (StatCost(stats[2]) > statPoints) { lukeff = -90m; }
-                }
+            
 
-                StatsDisplay(stats, statPoints, talentPoints);
-                
-                return stats;
-            }
+            
 
-            void StatsDisplay(List<int> stats, int statPoints, int talentPoints)
-            {
-                Console.WriteLine($"Stats:");
-                if (stats[0] != 0) { Console.WriteLine($"Int = {stats[0]}"); }
-                if (stats[1] != 0) { Console.WriteLine($"Dex = {stats[1]}"); }
-                if (stats[2] != 0) { Console.WriteLine($"Luk = {stats[2]}"); }
-                Console.WriteLine($"StatPoints left = {statPoints}");
+            
 
-                if (stats[3] != 0)
-                {
-                    Console.WriteLine("Talents:");
-                    Console.WriteLine($"CRT = {stats[3]}");
-                    Console.WriteLine($"TalentPoints left = {talentPoints}");
-                }
-            }
-
-            void CreationChances(List<int> stats, int classint, int job)
-            {
-                Console.WriteLine("--Creation chances--");
-
-                if (classint >= 1)
-                {
-                    decimal ppchance = 30 + 10 + 5 + job * 0.2m + stats[0] * 0.05m + stats[1] * 0.1m + stats[2] * 0.1m;
-                    decimal ppchancemin = ppchance - 15;
-                    decimal ppchancemax = ppchance - 5;
-                    Console.WriteLine($"Prepare potion chance: {ppchancemin} ~ {ppchancemax} %");
-                }
-
-                if (classint >= 3)
-                {
-                    decimal spchance = stats[0] + (stats[1] / 2) + stats[2] + job + 50 + (baselevel - 100);
-                    decimal spchancemin = spchance + 30 + 40;
-                    decimal spchancemax = spchance + 150 + 100;
-                    Console.WriteLine($"Special pharmacy creation: {spchancemin} ~ {spchancemax}");
-
-                    decimal mcchance = (job / 4) + (stats[1] / 3) + (stats[2] / 2);
-                    decimal mcchancemin = mcchance - 150 - 15;
-                    decimal mcchancemax = mcchance - 30 - 15;
-                    Console.WriteLine($"Mixed Cooking creation: {mcchancemin} ~ {mcchancemax}");
-                }
-
-                if (classint == 4)
-                {
-                    decimal bpchance = ((baselevel - 200) * 4) + stats[0] + stats[2] + (stats[3] * 2) + 100;
-                    decimal bpchancemin = bpchance + 300;
-                    decimal bpchancemax = bpchance + 400;
-                    Console.WriteLine($"Bionic pharmacy creation: {bpchancemin} ~ {bpchancemax}");
-                }
-            }
-
-            decimal StatEfficiency(int statcost, decimal multiplier)
-            {
-                return multiplier / statcost;
-            }
-
-            int StatCost(int attribute)
-            {
-                int cost = 999;
-                if (attribute < 130)
-                {
-                    if (attribute <= 99)
-                    {
-                        cost = (int)Math.Floor((decimal)(((attribute - 1) / 10) + 2));
-                    }
-                    else
-                    {
-                        cost = 4 * (int)Math.Floor((decimal)((attribute - 100) / 5)) + 16;
-                    }
-                }
-                return cost;
-            }
-
-            void OptimizeBuild(int buildint, int classint, List<int> stats)
-            {
-                decimal dexmulti;
-                decimal intmulti;
-                decimal lukmulti;
-                int statPoints = 0;
-                int talentPoints = 0;
-                switch (classint)
-                {
-                    case 1:
-                        statPoints = 1273;
-                        joblevel = 50;
-                        baselevel = 99;
-                        break;
-
-                    case 2:
-                        statPoints = 1325;
-                        joblevel = 70;
-                        baselevel = 99;
-                        break;
-
-                    case 3:
-                        statPoints = 2597;
-                        joblevel = 70;
-                        baselevel = 200;
-                        break;
-
-                    case 4:
-                        statPoints = 4151;
-                        talentPoints = 97;
-                        stats[3] = 100;
-                        joblevel = 50;
-                        baselevel = 250;
-                        break;
-                }
-
-                switch (buildint)
-                {
-                    case 1:
-                        dexmulti = 0.1m;
-                        intmulti = 0.05m;
-                        lukmulti = 0.1m;
-                        stats = DistributePoints(stats, dexmulti, intmulti, lukmulti, statPoints, talentPoints);
-                        break;
-                    case 2:
-                        dexmulti = 0.5m;
-                        intmulti = 1m;
-                        lukmulti = 1m;
-                        stats = DistributePoints(stats, dexmulti, intmulti, lukmulti, statPoints, talentPoints);
-                        break;
-                    case 3:
-                        for (int i = 0; i < 129; i++)
-                        {
-                            statPoints -= StatCost(stats[2]);
-                            stats[2] += 1;
-                            statPoints -= StatCost(stats[0]);
-                            stats[0] += 1;
-                        }
-                        StatsDisplay(stats, statPoints, talentPoints);
-                        break;
-                }
-            }
+            
     }
 }
-
-
