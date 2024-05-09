985829027aa9d8e189f0c2f54dd72e9e6bd43254
Angelo-Pedott-apel <angelopedott@gmail.com>
2024-05-08 21:17:18 -0300

    initial commit

---
+++ Character.cs
@@ -0,0 +1,4 @@
+public class character(string name, string buildname, int buildint, string classname, int classint, List<int> stats, int joblvl, int baselvl,int statpoints, int talentPoints){
+
+
+}
